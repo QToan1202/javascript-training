90ce5699f408791361ef1f15d70d70a565ecf4f8
Quoc Toan <toan124677@gmail.com>
2022-07-01 16:57:56 +0700

    Practice with conditional

---
+++ example/conditional.js
@@ -0,0 +1,19 @@
+const a = 2;
+const b = 4;
+
+const str1 = 'Hello world';
+const str2 = 'Hello world'
+
+if (a > b) {
+  console.log("True")
+} else {
+  console.log("False")
+}
+
+(a > b) ? console.log("Hello") : console.log("Hmm");
+
+if (str1 === str2) {
+  console.log("Ok!")
+} else {
+  console.log("Not Ok!")
+}
