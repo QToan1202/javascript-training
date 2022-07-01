60af006dbf6335f657727cf308aea5c8d7cde2d5
Quoc Toan <toan124677@gmail.com>
2022-07-01 17:06:45 +0700

    Practice with string

---
+++ example/string.js
@@ -0,0 +1,14 @@
+const fullName = 'Nguyen Quoc Toan';
+const date_of_birth = '19/12/2002';
+const homeTown = "Hue";
+
+console.log('Hello my name is: ' + fullName);
+console.log(`Hello my name is: ${fullName} and my birthday is:  ${date_of_birth}`);
+console.log(`Come from ${homeTown}`);
+
+const paragraph = `Hello
+from
+the
+other
+side`;
+console.log(paragraph)
