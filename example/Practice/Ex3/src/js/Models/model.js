6a783ed6b0b342b87469510e5f7a7e61d4ea760c
Quoc Toan <toan124677@gmail.com>
2022-07-26 15:20:33 +0700

    Pomodoro app: update model method

--- example/Practice/Ex3/src/js/Models/model.js
+++ example/Practice/Ex3/src/js/Models/model.js
@@ -9,52 +9,58 @@ export default class Model {
     this.onPomodoroListChanged = callback;
   }
 
-  // Checking the item is already exist or not
+  /**
+   * Checking the item is already exist or not
+   * @param {string} taskName
+   * @returns boolean
+   */
   isExist(taskName) {
     return this.pomodoro.some((item) => item.taskName === taskName);
   }
 
-  // Add new task if validate success
+  /**
+   * Add new Task if don't exist any similar task before
+   * @param {string} taskName
+   * @param {number} pomodoroCount
+   */
   addTask(taskName, pomodoroCount) {
     if (!this.isExist(taskName)) {
       const task = new Task(taskName, 0, pomodoroCount, false);
       this.pomodoro.push(task);
-
       localStorage.setItem('pomodoro', JSON.stringify(this.pomodoro));
       this.onPomodoroListChanged(this.pomodoro);
     }
   }
 
-  // Delete task belong to the specified ID
+  /**
+   * Delete task belong to the specified ID
+   * @param {number} id
+   */
   deleteTask(id) {
     this.pomodoro = this.pomodoro.filter((item) => item.id !== id);
     localStorage.setItem('pomodoro', JSON.stringify(this.pomodoro));
     this.onPomodoroListChanged(this.pomodoro);
   }
 
-  // Increase the porodori by 1 also check if it finished or not
+  /**
+   * Increase the porodori by 1
+   * @param {number} id
+   */
   increaseTask(id) {
-    this.pomodoro = this.pomodoro.map((item) => {
-      if (item.id === id) {
-        item.pomodoroDone += 1;
-        if (item.pomodoroDone === item.pomodoroCount) {
-          item.finished = true;
-        }
-      }
-      return item;
-    });
+    const task = this.pomodoro.find((item) => item.id === id);
+    task.pomodoroDone += 1;
+    if (task.pomodoroCount === task.pomodoroDone) task.finished = true;
     localStorage.setItem('pomodoro', JSON.stringify(this.pomodoro));
     this.onPomodoroListChanged(this.pomodoro);
   }
 
-  // Mark task as finished
+  /**
+   * Mark task as finished
+   * @param {number} id
+   */
   doneTask(id) {
-    this.pomodoro = this.pomodoro.map((item) => {
-      if (item.id === id) {
-        item.finished = true;
-      }
-      return item;
-    });
+    const task = this.pomodoro.find((item) => item.id === id);
+    if (task.id === id) task.finished = true;
     localStorage.setItem('pomodoro', JSON.stringify(this.pomodoro));
     this.onPomodoroListChanged(this.pomodoro);
   }
