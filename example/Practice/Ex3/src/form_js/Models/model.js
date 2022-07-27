a6d6c0df3ab5f81843bde2498477df1a82b5642a
Quoc Toan <toan124677@gmail.com>
2022-07-27 14:27:20 +0700

    Form validation: Update function in model

--- example/Practice/Ex3/src/form_js/Models/model.js
+++ example/Practice/Ex3/src/form_js/Models/model.js
@@ -1,62 +1,54 @@
+import { regEmail, regName, regPass } from '../regex';
+import User from './user';
+
 export default class Model {
   constructor() {
     this.user = {};
     this.error = {};
-    // Allow special character + @ + domain
-    this.regEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    // Allow upper and lower case letter, space
-    this.regName = /^[A-Za-z ]+$/;
-    // Have at least 1 number, 1 special character and in the range from 6 to 16 characters
-    this.regPass = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
   }
 
   bindDataChanged(callback) {
     this.onDataChanged = callback;
   }
 
+  /**
+   * Validation form
+   * @param {string} email
+   * @param {string} name
+   * @param {string} password
+   * @param {string} confirmPassword
+   */
   validationForm(email, name, password, confirmPassword) {
     // Reset obj for another validate
     this.error = {};
     this.user = {};
 
     // Validate Email
-    if (!this.regEmail.test(email)) {
-      this.error.errorEmail = 'Email is required and must be at the right format';
-    }
+    if (!regEmail.test(email)) this.error.errorEmail = 'Email is required and must be at the right format';
 
     // Validate Name
-    if (!this.regName.test(name)) {
-      this.error.errorName = 'Name is requied and not accept any number or special characters';
-    }
+    if (!regName.test(name)) this.error.errorName = 'Name is requied and not accept any number or special characters';
 
     // Validate Password
-    if (!this.regPass.test(password)) {
-      this.error.errorPassword = 'Password is required and must have at least one number and special character';
-    }
+    if (!regPass.test(password)) this.error.errorPassword = 'Password is required and must have at least one number and special character';
 
     // Checking if Confirm Password and Password is match or not
-    this.checkConfirmPassword(password, confirmPassword);
+    if (password !== confirmPassword) this.error.errorConfirmPassword = "Don't match password";
     this.onDataChanged(this.user, this.error);
   }
 
-  checkConfirmPassword(password, confirmPassword) {
-    if (password !== confirmPassword) {
-      this.error.errorConfirmPassword = 'Don\'t match password';
-    }
-  }
-
-  // Adding a user
+  /**
+   * Add new user if don't have any error in validate process
+   * @param {string} email
+   * @param {string} name
+   * @param {string} password
+   * @param {string} confirmPassword
+   */
   addUser(email, name, password, confirmPassword) {
     // Validate before execute adding process
     this.validationForm(email, name, password, confirmPassword);
-    if (Object.keys(this.error).length === 0) {
-      const user = {
-        email,
-        name,
-        password,
-        confirmPassword,
-      };
-      this.user = user;
+    if (!Object.keys(this.error).length) {
+      this.user = new User(email, name, password, confirmPassword);
       this.onDataChanged(this.user, this.error);
     }
   }
