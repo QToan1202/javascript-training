3108b5a7d1d867a5b81e7d4120d64b3f1978a3ac
Quoc Toan <toan124677@gmail.com>
2022-08-04 13:45:48 +0700

    Resolve #4: Create task detail

--- big-practice/src/scripts/templates/task.js
+++ big-practice/src/scripts/templates/task.js
@@ -20,4 +20,45 @@ class Task {
       </div>
     </div>
   </div>`;
+
+  static renderDetailTask = (
+    id,
+    taskName,
+    desc,
+    dueDate,
+  ) => `<div class="card" id="${id}">
+  <div class="card-header">
+    <h2 class="title  title--card">${taskName}</h2>
+    <select id="js-state" class="card-header__state">
+      <option value="todo">Todo</option>
+      <option value="in-progress">In progress</option>
+      <option value="done">Done</option>
+      <option value="archived">Archived</option>
+    </select>
+    <button class="card-header__btn">&times;</button>
+  </div>
+
+  <div class="card-content">
+      <div class="row">
+        <div class="title">
+          <h3>Description</h3>
+          <img width="20" height="20" src="./assets/images/edit-icon.svg" alt="edit-icon" />
+        </div>
+        <p id="js-desc" class="desc" contenteditable="true">${desc}</p>
+      </div>
+
+      <div class="row">
+        <h3 class="title">Due Date</h3>
+        <div class="row  row--inline-center">
+          <p id="js-due-date">${dueDate}</p>
+          <p class="alert"></p>
+        </div>
+      </div>
+
+      <div class="row">
+        <h3 class="title  title--spacing">Comments</h3>
+        <textarea class="comments" id="js-comment" placeholder="Enter new comment..." rows="1"></textarea>
+      </div>
+  </div>
+  </div>`;
 }
