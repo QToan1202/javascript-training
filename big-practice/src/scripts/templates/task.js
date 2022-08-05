a6d5b702d443a29b0b1d154cdc7b90fc5f39d197
Quoc Toan <toan124677@gmail.com>
2022-08-05 14:03:23 +0700

    Change class name

--- big-practice/src/scripts/templates/task.js
+++ big-practice/src/scripts/templates/task.js
@@ -5,20 +5,17 @@ class Task {
     createdDate,
     dueDate,
   ) => `<div class="task" id="${id}">
-    <div class="task-content">
-      <div class="task-content--row">
-        <h2 class="task-content__title">${taskName}</h2>
-        <img
-          src="./assets/images/delete-icon.svg"
-          alt="delete-icon"
-          id="delete"
-        />
-      </div>
-      <div class="task-content--row">
-        <p>${createdDate}</p>
-        <p><img src="./assets/images/clock-icon.svg" alt="clock-icon" /> ${dueDate} </p>
-      </div>
+  <div class="task-content">
+    <div class="row  row--task">
+      <h2 class="task-content__title">${taskName}</h2>
+      <img class="push" src="./assets/images/delete-icon.svg" alt="delete-icon" id="delete" />
     </div>
+    <div class="row  row--task">
+      <p>${createdDate}</p>
+      <img class="push" src="./assets/images/clock-icon.svg" alt="clock-icon" />
+      <p> ${dueDate} </p>
+    </div>
+  </div>
   </div>`;
 
   static renderDetailTask = (
@@ -35,30 +32,30 @@ class Task {
       <option value="done">Done</option>
       <option value="archived">Archived</option>
     </select>
-    <button class="card-header__btn">&times;</button>
+    <button class="btn  push">&times;</button>
   </div>
 
   <div class="card-content">
-      <div class="row">
-        <div class="title">
-          <h3>Description</h3>
-          <img width="20" height="20" src="./assets/images/edit-icon.svg" alt="edit-icon" />
-        </div>
-        <p id="js-desc" class="desc" contenteditable="true">${desc}</p>
+    <div class="row  row--direction">
+      <div class="title">
+        <h3>Description</h3>
+        <img width="20" height="20" src="./assets/images/edit-icon.svg" alt="edit-icon" />
       </div>
+      <p id="js-desc" class="desc" contenteditable="true">${desc}</p>
+    </div>
 
-      <div class="row">
-        <h3 class="title">Due Date</h3>
-        <div class="row  row--inline-center">
-          <p id="js-due-date">${dueDate}</p>
-          <p class="alert"></p>
-        </div>
+    <div class="row  row--direction">
+      <h3 class="title">Due Date</h3>
+      <div class="row  row--center">
+        <p id="js-due-date">${dueDate}</p>
+        <p class="alert"></p>
       </div>
+    </div>
 
-      <div class="row">
-        <h3 class="title  title--spacing">Comments</h3>
-        <textarea class="comments" id="js-comment" placeholder="Enter new comment..." rows="1"></textarea>
-      </div>
+    <div class="row  row--direction">
+      <h3 class="title  title--spacing">Comments</h3>
+      <textarea class="comments" id="js-comment" placeholder="Enter new comment..." rows="1"></textarea>
+    </div>
   </div>
   </div>`;
 }
