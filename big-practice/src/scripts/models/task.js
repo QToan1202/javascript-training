fd8e2bb29a37e8695a3196ce630882769284665a
Quoc Toan <toan124677@gmail.com>
2022-08-29 11:26:08 +0700

    Update class Task

--- big-practice/src/scripts/models/task.js
+++ big-practice/src/scripts/models/task.js
@@ -1,11 +1,12 @@
 import date from '../utilities/date';
 
 export default class Task {
-  constructor(taskName) {
+  constructor(taskName, userId) {
     this.taskName = taskName;
     this.description = '';
     this.dueDate = date.getTemporaryDueDate();
     this.createdDate = date.getCurrentDate();
     this.stateId = 1;
+    this.userId = userId;
   }
 }
