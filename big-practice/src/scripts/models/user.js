26015feb27459cb0037b2c692e49682a0a4a1fe7
Quoc Toan <toan124677@gmail.com>
2022-08-29 11:26:24 +0700

    Creat user model

---
+++ big-practice/src/scripts/models/user.js
@@ -0,0 +1,7 @@
+export default class User {
+  constructor(userName, password) {
+    this.userName = userName;
+    this.password = password;
+    this.avatar = 'https://picsum.photos/200';
+  }
+}
