c5523fd78233913e60352d1517aaeb6d1d323c2b
Quoc Toan <toan124677@gmail.com>
2022-09-08 11:34:08 +0700

    Update avatar attribute in user

--- big-practice/src/scripts/models/user.js
+++ big-practice/src/scripts/models/user.js
@@ -2,6 +2,6 @@ export default class User {
   constructor(userName, password) {
     this.userName = userName;
     this.password = password;
-    this.avatar = 'https://picsum.photos/200';
+    this.avatar = `https://picsum.photos/id/${Math.round(Math.random() * 1000) + 1}/200`;
   }
 }
