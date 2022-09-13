ee592493c89a9f62623aad428b0b90f00379fd00
Quoc Toan <toan124677@gmail.com>
2022-09-13 16:09:32 +0700

    Update naming

--- big-practice/src/scripts/index.js
+++ big-practice/src/scripts/index.js
@@ -1,4 +1,4 @@
-import Controller from './controllers/controllerTask';
+import Controller from './controllers/taskController';
 import TaskModel from './models/taskModel';
 import ModalDetail from './views/modalDetail';
 import TaskView from './views/taskView';
