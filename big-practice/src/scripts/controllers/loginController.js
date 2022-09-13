30f19027b08d40d81eca7277ce5195eb5bc8a38b
Quoc Toan <toan124677@gmail.com>
2022-09-13 16:09:10 +0700

    Update model, view name in loginController

--- big-practice/src/scripts/controllers/loginController.js
+++ big-practice/src/scripts/controllers/loginController.js
@@ -1,26 +1,26 @@
 export default class ControllerLogin {
-  constructor(model, view) {
-    this.model = model;
-    this.view = view;
+  constructor(userModel, loginView) {
+    this.userModel = userModel;
+    this.loginView = loginView;
 
     this.init();
   }
 
   init = async () => {
-    this.view.bindLoginUser(this.handlerLoginUser);
-    this.view.renderForm();
-    this.view.bindCreateAccount(this.handlerCreateAccount);
-    await this.model.getUsers();
+    this.loginView.bindLoginUser(this.handlerLoginUser);
+    this.loginView.renderForm();
+    this.loginView.bindCreateAccount(this.handlerCreateAccount);
+    await this.userModel.getUsers();
   };
 
   handlerLoginUser = async (userName, password) => {
-    const hasLogin = await this.model.loginUser(userName, password);
-    this.view.redirectToHome(hasLogin);
+    const hasLogin = await this.userModel.loginUser(userName, password);
+    this.loginView.redirectToHome(hasLogin);
   };
 
   handlerCreateAccount = async (userName, password) => {
-   const isSuccess = await this.model.createAccount(userName, password);
-   this.view.alertError(isSuccess);
-   this.view.redirectToHome(isSuccess);
+   const isSuccess = await this.userModel.createAccount(userName, password);
+   this.loginView.alertError(isSuccess);
+   this.loginView.redirectToHome(isSuccess);
   };
 }
