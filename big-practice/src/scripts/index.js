9974eddf2d1cd629922ecd937196362e7591b9d2
Quoc Toan <toan124677@gmail.com>
2022-09-22 08:44:43 +0700

    Rename import controller

--- big-practice/src/scripts/index.js
+++ big-practice/src/scripts/index.js
@@ -1,6 +1,6 @@
-import Controller from './controllers/taskController';
+import TaskController from './controllers/taskController';
 import TaskModel from './models/taskModel';
 import ModalDetail from './views/modalDetail';
 import TaskView from './views/taskView';
 
-const app = new Controller(new TaskModel(), new TaskView(), new ModalDetail());
+const app = new TaskController(new TaskModel(), new TaskView(), new ModalDetail());
