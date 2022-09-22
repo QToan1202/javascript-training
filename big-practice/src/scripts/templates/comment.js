4795b08e00d40bc12821d7a0e9c89fec08d0439a
Quoc Toan <toan124677@gmail.com>
2022-09-22 08:44:18 +0700

    Update style in comment section

--- big-practice/src/scripts/templates/comment.js
+++ big-practice/src/scripts/templates/comment.js
@@ -2,8 +2,8 @@ import deleteIcon from '../../assets/images/delete-icon.svg';
 
 export default class Comment {
   static renderComment = (avatar, userName, content, id) => (
-    `<div class="row  row--center">
-        <img class="avatar" src="${avatar}" alt="avatar" />
+    `<div class="row  row--center  js-comment-field  spacing">
+        <img class="avatar  clear-left" src="${avatar}" alt="avatar" />
         <div class="row row--direction">
           <h3 class="name">${userName}</h3>
           <p>${content}</p>
