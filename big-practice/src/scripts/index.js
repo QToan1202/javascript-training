9be114e66957b84101878c01439c295df853fb91
Quoc Toan <toan124677@gmail.com>
2022-09-26 08:31:25 +0700

    Rename js module

--- big-practice/src/scripts/index.js
+++
@@ -1,6 +0,0 @@
-import TaskController from './controllers/taskController';
-import TaskModel from './models/taskModel';
-import ModalDetail from './views/modalDetail';
-import TaskView from './views/taskView';
-
-new TaskController(new TaskModel(), new TaskView(), new ModalDetail());
